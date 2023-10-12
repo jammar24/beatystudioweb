43dce871aa3e9bdc1f39c4da4662a9176a566cde
jammar24 <jamarodalis@yahoo.com>
2023-10-11 23:37:52 -0500

    styles,about

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,22 +1,46 @@
 import 'react'
+import dataTiktok from "../helpers/datatiktok";
+import './styles/about.css'
 
 const About = () => {
+
+  function TikTokVideo({ video}) {
+    return (
+      <div>
+         <iframe
+          title="Video de TikTok"
+          width="300"
+          height="400"
+          src={video.video} 
+          frameBorder="0"
+        />
+        <a href={video.video} target="_blank" rel="noopener noreferrer">
+   
+        </a>
+      </div>
+    );
+  }
+
+
+
   return (
-    <article>
+    <article  id='about'>
         <div>
-           <span></span><h3>PIXEL Skincare</h3>
+           <span></span><h3>Beauty Studio</h3>
         </div>
- <div>
-    <div>
-    <p>Since 2016, Pixel Skin Care has been at the forefront of the move towards organic and natural skincare.</p>
+ 
+    <div  className='about__text'>
+    <p>Since 2016, Beauty Studio has been at the forefront of the move towards organic and natural skincare.</p>
       <p>Specialising in emerging niche natural skin care brands, Pixel Skin Care is a safe zone for you and your skin where we have taken special care to hand pick and offer you some of the purest and safest brands from global organic specialists.</p>
       <p>From cutting edge science to luxurious natural indulgence, we have selected the best products and treatments for healing and improving your well-being.</p>
     </div>
-
-    <div>
-    <iframe width="560" height="315" src="https://www.youtube.com/embed/otej7WLdPh0" title="YouTube video player"  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    <div  className='about__vdo'>
+    {dataTiktok.map((video, data) => (
+        <TikTokVideo key={data} video={video} />
+      ))}
     </div>
- </div>
+    
+
     </article>
   )
 }
---
+++ src/helpers/datatiktok.jsx
@@ -0,0 +1,12 @@
+const dataTiktok = [
+     {
+        "title": "Video 1",
+        "author": "Usuario 1",
+        "video": "https://www.tiktok.com/@beauty_studio.della/video/7282912232281885957",
+        "likes": 1000
+      }
+      
+
+ ]
+export default dataTiktok;
+  
